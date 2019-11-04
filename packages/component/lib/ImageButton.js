effc40c950a0d6ab023e2f23f819848d76eed040
Kuyen <iamcxa@gmail.com>
2019-11-04 12:19:09 +0800

    fix: 🐛 missing import
    fix missing Screen import

--- packages/component/lib/ImageButton.js
+++ packages/component/lib/ImageButton.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Image } from 'react-native';
+import { Screen, ScaledSheet } from '@ublocks-react-native/helper';
 
 import RoundButton from './RoundButton';
 
