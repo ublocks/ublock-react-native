6c6bf14b5a3b9e66523493c383dd19f888acb055
Kuyen <iamcxa@gmail.com>
2019-12-13 20:20:50 +0800

    feat: 🎸 add new style and new props to LoadingIndicator
    add Animatable to give a short FadeIn animation, and add LinearGradient
    to give a linear-transparent background. also, add text props just in
    case to show the currrent loading reason.

--- packages/component/lib/LoadingIndicator.js
+++ packages/component/lib/LoadingIndicator.js
@@ -1,20 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { ActivityIndicator, View } from 'react-native';
-
+import * as Animatable from 'react-native-animatable';
+import { ActivityIndicator, View, Text } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 import { Screen, ScaledSheet } from '@ublocks-react-native/helper';
 
 const styles = ScaledSheet.create({
+  loadingCoverWrapper: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   loadingContent: {
     position: 'absolute',
     width: Screen.width,
     height: Screen.height,
-    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    // backgroundColor: 'gray',
-    // opacity: 0.1,
-    zIndex: 100,
+    backgroundColor: 'transparent',
+    zIndex: Number.MAX_SAFE_INTEGER,
   },
   loadingCover: {
     width: Screen.scale(100),
@@ -25,32 +28,80 @@ const styles = ScaledSheet.create({
     alignItems: 'center',
     alignSelf: 'center',
     alignContent: 'center',
-    zIndex: 101,
+    zIndex: Number.MAX_SAFE_INTEGER,
+  },
+  largeLoadingCover: {
+    width: 'auto',
+    height: 'auto',
+    minWidth: Screen.scale(100),
+    minHeight: Screen.scale(120),
+  },
+  txtLoadingMessage: {
+    fontWeight: 'bold',
+    color: 'white',
+    textAlign: 'center',
   },
 });
 
-export default class LoadingIndicator extends Component {
+export default class LoadingIndicator extends React.PureComponent {
   static propTypes = {
+    text: PropTypes.string,
     open: PropTypes.bool,
+    cover: PropTypes.bool,
+    width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   };
 
   static defaultProps = {
+    text: '',
     open: false,
+    cover: true,
+    width: Screen.width,
+    height: Screen.height,
   };
 
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+  renderIndicator = () => {
+    const { text, cover, width, height } = this.props;
+    return (
+      <Animatable.View
+        style={[
+          styles.loadingContent,
+          { width, height },
+          !cover && { backgroundColor: 'rgba(100,100,100,0.5)' },
+        ]}
+        animation="fadeIn"
+        duration={250}
+      >
+        <View
+          style={[
+            styles.loadingCoverWrapper,
+            cover && styles.loadingCover,
+            text && styles.largeLoadingCover,
+          ]}
+        >
+          <ActivityIndicator size="large" color={'white'} />
+          {text && <Text style={styles.txtLoadingMessage}>{text}</Text>}
+        </View>
+      </Animatable.View>
+    );
+  };
+
+  renderLinearGradient = () => {
+    const { width, height } = this.props;
+    return (
+      <LinearGradient
+        style={[styles.loadingContent, { width, height }]}
+        colors={['rgba(100,100,100,0.5)', '#ffffff00']}
+        start={{ x: 0, y: 0.8 }}
+        end={{ x: 0, y: 1 }}
+      >
+        {this.renderIndicator()}
+      </LinearGradient>
+    );
+  };
 
   render() {
-    const { open } = this.props;
-    return (
-      <View style={[styles.loadingContent, open ? {} : { height: 0, width: 0 }]}>
-        <View style={[styles.loadingCover, open ? {} : { height: 0, width: 0 }]}>
-          {open ? <ActivityIndicator size="large" color="white" /> : null}
-        </View>
-      </View>
-    );
+    const { cover, open } = this.props;
+    return open && (cover ? this.renderLinearGradient() : this.renderIndicator());
   }
 }
